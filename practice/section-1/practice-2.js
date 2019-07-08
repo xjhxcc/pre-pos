6e9120d79063ff5e88f6031f330803f0cc631ac0
xjhxcc <2351889342@qq.com>
2019-07-08 23:00:07 +0800

    1, 2.1

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var result=[];
+  for(var i=0;i<collectionA.length;i++){
+    for(var j=0;j<collectionB.length;j++){
+      for(var k=0;k<collectionB[j].length;k++){
+        if(collectionA[i]==collectionB[j][k]){
+          result.push(collectionA[i]);
+        } 
+      }     
+    }
+  }
+  return result;
 }
--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var collection_a=[];
+  var m={};
+  for (let i = 0; i <collection.length ; i++) {
+    if (collection[i] in m) {
+      m[collection[i]]=m[collection[i]] + 1;
+    }
+    else {
+      m[collection[i]]=1;
+    }
+  }
+  for (var key in m) {
+    var temp={};
+    temp['key']=key;
+    temp['count']=m[key];
+    collection_a.push(temp);
+  }
+   return collection_a;
+  return result;
 }
